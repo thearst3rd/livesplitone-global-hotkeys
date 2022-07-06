a86fca1c0925f4e5816f33edf517aca02c0dbf3f
Terry Hearst <thearst3rd@gmail.com>
2022-07-06 02:21:42 -0400

    Progress

--- lso-hotkeys.py
+++ lso-hotkeys.py
@@ -1,19 +1,45 @@
 import asyncio
 import websockets
-import global_hotkeys
+import threading
+from pynput import keyboard
+
 
 CONNECTIONS = set()
 
+
+def on_press(key):
+	print(type(key), key)
+	if key == keyboard.KeyCode.from_vk(96):
+		print("split")
+		websockets.broadcast(CONNECTIONS, "split")
+		websockets.broadcast(CONNECTIONS, "start")
+	elif key == 99:
+		print("reset")
+		websockets.broadcast(CONNECTIONS, "reset")
+	elif key == 98:
+		print("skip")
+		websockets.broadcast(CONNECTIONS, "skip")
+
+
 async def register(websocket):
-    CONNECTIONS.add(websocket)
-    try:
-        await websocket.wait_closed()
-    finally:
-        CONNECTIONS.remove(websocket)
+	CONNECTIONS.add(websocket)
+	print("websocket connected")
+	try:
+		await websocket.wait_closed()
+		print("websocket closed")
+	finally:
+		CONNECTIONS.remove(websocket)
+
 
 async def main():
-    async with websockets.serve(register, "localhost", 5678):
-        await asyncio.Future()
+	async with websockets.serve(register, "localhost", 5678):
+		await asyncio.Future()
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+	# Make keyboard listener thread
+	listener = keyboard.Listener(
+		on_press=on_press)
+	listener.start()
+
+	asyncio.run(main())
